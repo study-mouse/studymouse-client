a564efbe16fff46aa0d6e5aaf87933a658cf7e2d
samslow <shsvkdldj@likelion.org>
2020-07-18 17:02:18 +0900

    Adjust Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { fontDefault } from '../constants/styles';
 import * as styles from '../constants/styles';
@@ -8,20 +8,19 @@ const Navbar = styled.div`
   height: 5rem;
   position: fixed;
   width: 100%;
-  top: 0;
 
   background-color: ${styles.colors['dark_black']};
   border-bottom: 1px solid #333333;
 `;
 
-const ServiceName = styled(fontDefault)`
+const LogoContainer = styled.a`
   display: flex;
   align-items: center;
-  margin: 0 5rem 0 3rem;
-
-  font-size: 20px;
-  font-weight: 900;
-  color: white;
+  margin: 0 30px;
+  width: 240px;
+`;
+const Logo = styled.img`
+  height: 80%;
 `;
 
 const PageName = styled(fontDefault)`
@@ -34,11 +33,22 @@ const PageName = styled(fontDefault)`
 `;
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  useEffect(() => {
+    if (document.URL.split('/').includes('login')) setIsLogin(false);
+  });
+
   return (
     <Navbar>
-      <ServiceName>스터디마우스</ServiceName>
-      <PageName selected={true}>Word</PageName>
-      <PageName selected={false}>Archived</PageName>
+      <LogoContainer href="/">
+        <Logo src={require('../assets/logo.png')} />
+      </LogoContainer>
+      {isLogin && (
+        <>
+          <PageName selected>Word</PageName>
+          <PageName>Archived</PageName>
+        </>
+      )}
     </Navbar>
   );
 };
