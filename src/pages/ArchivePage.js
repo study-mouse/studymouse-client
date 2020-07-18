d80fd987ce786d767f81fc09c5416a337cebc7ef
samslow <shsvkdldj@likelion.org>
2020-07-19 00:25:54 +0900

    Add archive page

---
+++ src/pages/ArchivePage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import WordList from '../components/WordList';
+import * as mock from '../constants/mockData';
+
+const ArchivePage = () => {
+  const arrayGroupByKey = (array, key) => {
+    const groupedObj = array.reduce((result, curValue) => {
+      (result[curValue[key]] = result[curValue[key]] || []).push(curValue);
+      return result;
+    }, {});
+    const ordered = {};
+    Object.keys(groupedObj)
+      .sort()
+      .reverse()
+      .forEach((key) => {
+        ordered[key] = groupedObj[key];
+      });
+    return ordered;
+  };
+  return (
+    <Section>
+      <WordWrapper>
+        <WordList
+          sortedData={arrayGroupByKey(mock.mockData.datas, 'createdDate')}
+        />
+      </WordWrapper>
+    </Section>
+  );
+};
+
+const Section = styled.div`
+  display: flex;
+`;
+const WordWrapper = styled.div`
+  margin-top: 5rem;
+  flex-basis: 40rem;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 3rem;
+
+  background-color: #1a1a1a;
+`;
+
+export default ArchivePage;
