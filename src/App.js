f1d52f5da11e8c36c4384967ae1b6c0f066b9279
samslow <shsvkdldj@likelion.org>
2020-07-18 17:01:41 +0900

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Header from './components/Header';
-import WordSection from './components/WordSection';
+import WordPage from './pages/WordPage';
+import LoginPage from './pages/LoginPage';
 
 const Wrapper = styled.div`
   height: 100%;
 `;
 
-const Section = styled.div`
-  display: flex;
-`;
-
 const App = () => {
   return (
-    <Wrapper>
-      <Header />
-      <Section>
-        <WordSection />
-      </Section>
-    </Wrapper>
+    <BrowserRouter>
+      <Wrapper>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={WordPage} />
+          <Route path="/archive" component={WordPage} />
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+      </Wrapper>
+    </BrowserRouter>
   );
 };
 
